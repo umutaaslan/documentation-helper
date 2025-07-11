f9f2fe822c0f222efe540c1b458e709a19c8cfa5
umutaaslan <umutalperaslan705@hotmail.com>
2025-07-11 15:57:15 +0300

    add ui and functionality

--- backend/run_llm.py
+++ backend/run_llm.py
@@ -2,12 +2,15 @@ import os
 from langchain_google_vertexai import ChatVertexAI
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains import history_aware_retriever
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langsmith import Client
-from langchain.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate, ChatPromptTemplate
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 
-def run_llm(query):
+
+def run_llm(query, chat_history):
 
     llm = ChatVertexAI(model_name="gemini-2.5-flash")
     client = Client(api_key=os.environ["LANGSMITH_API_KEY"])
@@ -19,7 +22,7 @@ def run_llm(query):
 
     retrieval_prompt_template = """
     You are a documentation helper. Be practical and always try your best to help the user about their questions based on the documentation.
-
+    
     Answer any use questions based solely on the context below:
 
     <context>
@@ -31,17 +34,29 @@ def run_llm(query):
 
     retrieval_prompt = PromptTemplate.from_template(template=retrieval_prompt_template)
 
-    # retrieval_qa_prompt = client.pull_prompt("langchain-ai/retrieval-qa-chat")
+    rephrase_prompt = client.pull_prompt("langchain-ai/chat-langchain-rephrase")
 
-    combine_docs_chain = create_stuff_documents_chain(llm, prompt=retrieval_prompt)
-    retrieval_chain = create_retrieval_chain(
-        retriever=vector_store.as_retriever(search_kwargs={"k": 12}), combine_docs_chain=combine_docs_chain
+    history_aware_retriever = create_history_aware_retriever(
+        llm, vector_store.as_retriever(search_kwargs={"k": 12}), rephrase_prompt
     )
 
-    res = retrieval_chain.invoke({"input": query})
+    combine_docs_chain = create_stuff_documents_chain(llm, prompt=retrieval_prompt)
+
+    retrieval_chain = create_retrieval_chain(
+        retriever=history_aware_retriever,
+        combine_docs_chain=combine_docs_chain,
+    )
+
+    res = retrieval_chain.invoke({"input": query, "chat_history": chat_history})
     return res
 
 
 if __name__ == "__main__":
-    res = run_llm("what are the ways of memory implementation in langchain")
-    print(res["answer"])
+    while True:   
+        user_input = input("input: ")
+        if(user_input == "/bye"):
+            break
+
+        hist = [{"role":"user","content":"what is langchain"},{"role":"assistant","content":"Based solely on the provided context, there is no explicit definition of what \"LangChain\" is. The context primarily details different modules, classes, and functionalities available within the LangChain Python API, such as \"Chains\" and \"LangSmith utilities.\" \n\n --Resources-- \n\n\n\n https://python.langchain.com/v0.2/api_reference/nomic/index.html\n\nhttps://python.langchain.com/api_reference/langchain/chains.html\n\nhttps://python.langchain.com/v0.2/api_reference/langchain/chains.html\n\nhttps://python.langchain.com/api_reference/cohere/index.html\n\nhttps://python.langchain.com/api_reference/snowflake/index.html\n\nhttps://python.langchain.com/v0.2/api_reference/community/chains.html\n\nhttps://python.langchain.com/v0.2/api_reference/_modules/index.html\n\nhttps://python.langchain.com/v0.2/api_reference/airbyte/index.html\n\nhttps://python.langchain.com/v0.2/api_reference/ai21/llms.html\n\nhttps://python.langchain.com/api_reference/community/chains.html\n\nhttps://python.langchain.com/api_reference/langchain/smith.html\n\nhttps://python.langchain.com/v0.2/api_reference/langchain/smith.html"}]
+        res = run_llm(user_input, hist)
+        print(res["answer"])
--- main.py
+++ main.py
@@ -18,12 +18,15 @@ if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
     with st.spinner("Wait for it...", show_time=True):
-        answer = run_llm(prompt)
+        answer = run_llm(query=prompt, chat_history=st.session_state.messages)
         urls = []
-        for doc in answer["context"]:
-            urls.append(doc.metadata["source"])
+        for index, doc in enumerate(answer["context"]):
+            urls.append(f"{index}. {doc.metadata["source"]}")
         
-        full_answer = f"{answer['answer']} \n\n --Resources-- \n\n\n\n {"\n\n".join(urls)}"
+        full_answer = f"{answer['answer']}"
         st.session_state.messages.append({"role": "assistant", "content": full_answer})
         with st.chat_message("assistant"):
             st.markdown(full_answer)
+            expander = st.expander("Resources")
+            expander.write("\n\n".join(urls))
+    
